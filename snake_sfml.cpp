389a99f1bd0533a7b28dcc8a68c0f8e670143ae3
Egrvigrf <engraveingrief@gmail.com>
2025-01-20 05:16:16 +0800

    更新snake_sfml适应sfml3.0

--- snake_sfml.cpp
+++ snake_sfml.cpp
@@ -15,7 +15,8 @@ map<char, char> collide = { {'a','d'},{'d','a'},{'w','s'},{'s','w'} };
 const int gridSize = 40; // 单位方格大小
 const int windowWidth = (cols+2)*gridSize;
 const int windowHeight = (rows+2)*gridSize;
-sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "greedysnake");
+    // 使用 sf::Vector2u 创建 VideoMode
+sf::RenderWindow window(sf::VideoMode(sf::Vector2u(windowWidth, windowHeight)), "greedysnake");
 void print_x_y(int x, int y, char c) {
     sf::RectangleShape block(sf::Vector2f(gridSize, gridSize));
     if (x == fruit_x && y == fruit_y) {
@@ -29,7 +30,7 @@ void print_x_y(int x, int y, char c) {
     } else {
         block.setFillColor(sf::Color::Green); // 蛇身绿色
     }
-    block.setPosition(x * gridSize, y * gridSize);
+    block.setPosition(sf::Vector2f((float)x*gridSize, (float)y*gridSize));
     window.draw(block);
 }
 void print() {
@@ -53,21 +54,30 @@ void fruit_generate() {
 }
 void move() {
     sf::Clock clock;  // 创建一个时钟对象
-    sf::Event event;
+    // sf::Event event;
     char dir = snk_dir;
-    while (clock.getElapsedTime().asSeconds() < speed / 1000.0) {
-        if (window.pollEvent(event) && event.type == sf::Event::KeyPressed) {
-            if (event.key.code == sf::Keyboard::W) {
-                dir = 'w';  // 向上
-            } else if (event.key.code == sf::Keyboard::A) {
-                dir = 'a';  // 向左
-            } else if (event.key.code == sf::Keyboard::S) {
-                dir = 's';  // 向下
-            } else if (event.key.code == sf::Keyboard::D) {
-                dir = 'd';  // 向右
+    std::optional<sf::Event> event; 
+    // event = window.pollEvent();  // 获取事件
+        while (clock.getElapsedTime().asSeconds() < speed / 1000.0) {
+            const std::optional<sf::Event> event = window.pollEvent();
+            // 检查是否关闭窗口
+            // 处理键盘按下事件
+            if (event->is<sf::Event::KeyPressed>()) {
+                const auto& keyEvent = event->getIf<sf::Event::KeyPressed>();
+                if (keyEvent) {
+                    if (keyEvent->code == sf::Keyboard::Key::W) {
+                        dir = 'w';  // 向上
+                    } else if (keyEvent->code == sf::Keyboard::Key::A) {
+                        dir = 'a';  // 向左
+                    } else if (keyEvent->code == sf::Keyboard::Key::S) {
+                        dir = 's';  // 向下
+                    } else if (keyEvent->code == sf::Keyboard::Key::D) {
+                        dir = 'd';  // 向右
+                    }
+                }
             }
         }
-    }
+
     if (!(collide[dir] == snk_dir)) {
         snk_dir = dir;
     }
@@ -117,13 +127,14 @@ void game_init() {
 }
 int main() {
     game_init();
+
     while (alive) {
         if (!fruit_is_exist) fruit_generate();
         move();
         print();
     }
     cout << "Game over!\n length: " <<snk_len << endl;
-    Snap(3000);
+    Snap(1000);
     window.close();
     return 0;
 }
